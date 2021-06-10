adb17107782c381f9d06a1d4ee288b7719bc99ec
RODRIGO Varela <rodrigooliveiravarela@gmail.com>
2021-06-09 23:00:43 -0300

    Configurado rota da aplicação e adicionado menu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,29 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app.routing';
+
 import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     MenuModule,
+    MenubarModule,
+    InputTextModule,
+    ButtonModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
